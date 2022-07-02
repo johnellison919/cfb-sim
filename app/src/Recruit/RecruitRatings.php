cb365d9577dd482b185eefb92a460b71847e7707
John Ellison <johnellison919@gmail.com>
2022-07-01 23:13:16 -0500

    Adds RecruitRatings and RecruitRatingsModel
    Adds the RecruitRatings class and the RecruitRatingsModel class

---
+++ app/src/Recruit/RecruitRatings.php
@@ -0,0 +1,23 @@
+<?php
+
+	namespace Recruit;
+
+	use Nox\ORM\Interfaces\ModelInstance;
+	use Nox\ORM\Interfaces\MySQLModelInterface;
+	use Nox\ORM\ModelClass;
+
+	class RecruitRatingsRatings extends ModelClass implements ModelInstance
+	{
+		public int $recruitID;
+		// TODO: add different recruit ratings like speed, throwing, etc.
+
+		public static function getModel(): MySQLModelInterface
+		{
+			return new RecruitRatingsModel();
+		}
+
+		public function __construct()
+		{
+			parent::__construct($this);
+		}
+	}
---
+++ app/src/Recruit/RecruitRatingsModel.php
@@ -0,0 +1,53 @@
+<?php
+
+	namespace Recruit;
+
+	use Nox\ORM\Attributes\Model;
+	use Nox\ORM\ColumnDefinition;
+	use Nox\ORM\Interfaces\MySQLModelInterface;
+	use Nox\ORM\MySQLDataTypes\Integer;
+	use NoxEnv;
+
+	#[Model]
+	class RecruitRatingsModel implements MySQLModelInterface
+	{
+		/**
+		 * The name of the database this table belongs to
+		 */
+		private string $mysqlDatabaseName = NoxEnv::MYSQL_DB_NAME;
+
+		/**
+		 * The name of this Model in the MySQL database as a table
+		 */
+		private string $mysqlTableName = "recruits_ratings";
+
+		/**
+		 * The string name of the class this model represents and can instantiate
+		 */
+		private string $representingClassName = Recruit::class;
+
+		public function getDatabaseName(): string{
+			return $this->mysqlDatabaseName;
+		}
+
+		public function getName(): string{
+			return $this->mysqlTableName;
+		}
+
+		public function getInstanceName(): string{
+			return $this->representingClassName;
+		}
+
+		public function getColumns(): array
+		{
+			return [
+				new ColumnDefinition(
+					name: "recruit_id",
+					classPropertyName: "recruitID",
+					dataType: new Integer(),
+					defaultValue: 0,
+					isUnique: true,
+				),
+			];
+		}
+	}
