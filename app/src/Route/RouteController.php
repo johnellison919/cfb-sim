f129c4b921d7a1f2e60b8bf7ec0640b243722678
John Ellison <johnellison919@gmail.com>
2022-07-03 15:57:36 -0500

    Replaces HomeController
    Replaces HomeController and related view

---
+++ app/src/Route/RouteController.php
@@ -0,0 +1,25 @@
+<?php
+
+	namespace Route;
+
+	use Nox\RenderEngine\Exceptions\LayoutDoesNotExist;
+	use Nox\RenderEngine\Exceptions\ParseError;
+	use Nox\RenderEngine\Exceptions\ViewFileDoesNotExist;
+	use Nox\RenderEngine\Renderer;
+	use Nox\Router\Attributes\Controller;
+	use Nox\Router\Attributes\Route;
+	use Nox\Router\BaseController;
+
+	#[Controller]
+	class RouteController extends BaseController{
+
+		/**
+		 * @throws ParseError
+		 * @throws ViewFileDoesNotExist
+		 * @throws LayoutDoesNotExist
+		 */
+		#[Route("GET", "/")]
+		public function mainView(): string{
+			return Renderer::renderView("main.php");
+		}
+	}
