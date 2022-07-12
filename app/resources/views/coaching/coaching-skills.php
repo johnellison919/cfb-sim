7b0cd164bdc026a43492a147ed0159ae1fff3733
John Ellison <johnellison919@gmail.com>
2022-07-11 22:51:18 -0500

    Adds Coaching Views
    Adds views for the coaching tab pages

---
+++ app/resources/views/coaching/coaching-skills.php
@@ -0,0 +1,12 @@
+@Layout = "base.php"
+@Head{
+	<title>Coaching Skills</title>
+}
+@Body{
+	<?php include __DIR__ . "/../_partials/navigation.php" ?>
+	<main class="container">
+		<section>
+			Coaching Skills
+		</section>
+	</main>
+}
---
+++ app/resources/views/coaching/coaching-style.php
@@ -0,0 +1,12 @@
+@Layout = "base.php"
+@Head{
+	<title>Coaching Style</title>
+}
+@Body{
+	<?php include __DIR__ . "/../_partials/navigation.php" ?>
+	<main class="container">
+		<section>
+			Coaching Style
+		</section>
+	</main>
+}
