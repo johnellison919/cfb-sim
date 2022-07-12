69a5f43f14478de9c7702f0a51e75459b4ee3fc9
John Ellison <johnellison919@gmail.com>
2022-07-11 22:52:23 -0500

    Adds Team Views
    Adds views for the team tab

---
+++ app/resources/views/team/depth-chart.php
@@ -0,0 +1,12 @@
+@Layout = "base.php"
+@Head{
+	<title>Depth Chart</title>
+}
+@Body{
+	<?php include __DIR__ . "/../_partials/navigation.php" ?>
+	<main class="container">
+		<section>
+			Depth Chart
+		</section>
+	</main>
+}
---
+++ app/resources/views/team/injury-report.php
@@ -0,0 +1,12 @@
+@Layout = "base.php"
+@Head{
+	<title>Injury Report</title>
+}
+@Body{
+	<?php include __DIR__ . "/../_partials/navigation.php" ?>
+	<main class="container">
+		<section>
+			Injury Report
+		</section>
+	</main>
+}
